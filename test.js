09f690bd95e28b5179a476fa02cd16e470ab9dd3
Titus Wormer <tituswormer@gmail.com>
2017-01-25 17:30:48 +0100

    Internalise intersection
    ...and add more tests (reaching 100% coverage).

--- test.js
+++ test.js
@@ -39,17 +39,129 @@ tape('unist-util-find-all-between', function (test) {
         type: 'foo',
         children: [{
           type: 'bar'
-        }, 
+        },
         {
           type: 'baz'
         }]
       }, 0, 1, false)
     }, 'Expected function, string, or object as test')
-  }, 'Should fail without test')
+  }, 'Should fail with invalid test')
+
+  test.doesNotThrow(function () {
+    test.deepEqual(
+      findAllBetween({
+        type: 'foo',
+        children: [{
+          type: 'bar'
+        },
+        {
+          type: 'baz'
+        }]
+      }, 0, 1),
+      [{type: 'bar'}]
+    )
+
+    test.deepEqual(
+      findAllBetween({
+        type: 'foo',
+        children: [{
+          type: 'bar'
+        },
+        {
+          type: 'baz'
+        }]
+      }, 0, 2),
+      [{type: 'bar'}, {type: 'baz'}]
+    )
+  }, 'Should support no test')
+
+  test.doesNotThrow(function () {
+    test.deepEqual(
+      findAllBetween({
+        type: 'foo',
+        children: [{
+          type: 'bar'
+        },
+        {
+          type: 'baz'
+        }]
+      }, 0, 1, 'bar'),
+      [{type: 'bar'}]
+    )
+
+    test.deepEqual(
+      findAllBetween({
+        type: 'foo',
+        children: [{
+          type: 'bar'
+        },
+        {
+          type: 'baz'
+        }]
+      }, 0, 1, 'baz'),
+      []
+    )
+  }, 'Should support `string` test')
+
+  test.doesNotThrow(function () {
+    test.deepEqual(
+      findAllBetween({
+        type: 'foo',
+        children: [{
+          type: 'bar'
+        },
+        {
+          type: 'baz'
+        }]
+      }, 0, 1, function (node) {
+        return node.type === 'bar'
+      }),
+      [{type: 'bar'}]
+    )
+
+    test.deepEqual(
+      findAllBetween({
+        type: 'foo',
+        children: [{
+          type: 'bar'
+        },
+        {
+          type: 'baz'
+        }]
+      }, 0, 1, function (node) {
+        return node.type === 'baz'
+      }),
+      []
+    )
+  }, 'Should support `function` test')
+
+  test.doesNotThrow(function () {
+    test.deepEqual(
+      findAllBetween({
+        type: 'foo',
+        children: [{
+          type: 'bar'
+        },
+        {
+          type: 'baz'
+        }]
+      }, 0, 1, {type: 'bar'}),
+      [{type: 'bar'}]
+    )
+
+    test.deepEqual(
+      findAllBetween({
+        type: 'foo',
+        children: [{
+          type: 'bar'
+        },
+        {
+          type: 'baz'
+        }]
+      }, 0, 1, {type: 'baz'}),
+      []
+    )
+  }, 'Should support `object` test')
 
   test.end()
 })
-
-
-
-
